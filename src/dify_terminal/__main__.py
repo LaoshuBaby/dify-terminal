bce67239b7fba94f26d0f495002f0da302e57e58
boholder <bottleholder@anche.no>
2024-05-04 22:13:50 +0800

    feat: achieve simple round chatting

--- src/dify_terminal/__main__.py
+++ src/dify_terminal/__main__.py
@@ -1,18 +1,29 @@
 import json
+import os
 
 from dify_client import ChatClient
 
-api_key = "your_api_key"
+api_key = os.environ["DIFY_LOCAL_API_KEY"]
 
 # Initialize ChatClient
 chat_client = ChatClient(api_key)
+chat_client.base_url = "http://localhost/v1"
 
-# Create Chat Message using ChatClient
-chat_response = chat_client.create_chat_message(inputs={}, query="Hello", user="user_id", response_mode="streaming")
-chat_response.raise_for_status()
+while True:
+    query = input("输入：")
+    if not query:
+        continue
 
-for line in chat_response.iter_lines(decode_unicode=True):
-    line = line.split('data:', 1)[-1]
-    if line.strip():
-        line = json.loads(line.strip())
-        print(line.get('answer'))
+    chat_response = chat_client.create_chat_message(inputs={}, query=query, user="user_id", response_mode="streaming")
+    chat_response.raise_for_status()
+
+    accumulated_answer = ""
+    for line in chat_response.iter_lines(decode_unicode=True):
+        line = line.split('data:', 1)[-1]
+        if line.strip():
+            line = json.loads(line.strip())
+            if answer := line.get('answer'):
+                accumulated_answer += answer
+                print("\r", accumulated_answer, end="")
+
+    print("")
