eb5918f4222e4390b2efbf0a5590948ecb821975
boholder <bottleholder@anche.no>
2024-05-05 10:25:57 +0800

    build: add pre-commit for linting

--- src/dify_terminal/__main__.py
+++ src/dify_terminal/__main__.py
@@ -13,27 +13,28 @@ chat_client.base_url = "http://localhost/v1"
 
 def user_say(what: str):
     global conversation_id
-    chat_response = chat_client.create_chat_message(inputs={}, query=what, user="dify_terminal", conversation_id=conversation_id,
-                                                    response_mode="streaming")
+    chat_response = chat_client.create_chat_message(
+        inputs={}, query=what, user="dify_terminal", conversation_id=conversation_id, response_mode="streaming"
+    )
     chat_response.raise_for_status()
     accumulated_answer = ""
     for line in chat_response.iter_lines(decode_unicode=True):
         # line="event: ping" when starting the LLM
         if not line.startswith("data:"):
             continue
-        data: str = line.split('data:', 1)[-1]
+        data: str = line.split("data:", 1)[-1]
         if data := data.strip():
             dic = json.loads(data)
 
             if conversation_id is None:
-                conversation_id = dic.get('conversation_id')
+                conversation_id = dic.get("conversation_id")
 
-            if answer := dic.get('answer'):
+            if answer := dic.get("answer"):
                 accumulated_answer += answer
                 print("\r", accumulated_answer, end="")
 
 
-if __name__ == "__main__":
+def main():
     while True:
         query = input(": ")
         if not query:
@@ -41,3 +42,7 @@ if __name__ == "__main__":
         user_say(query)
         # start a new line
         print("")
+
+
+if __name__ == "__main__":
+    main()
