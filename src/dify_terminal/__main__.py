81c52ee5771c5cb2b60aa1fd4ac4c30acf5ad0e7
boholder <bottleholder@anche.no>
2024-05-05 11:19:13 +0800

    fix: fix repeat response part in cmd

--- src/dify_terminal/__main__.py
+++ src/dify_terminal/__main__.py
@@ -1,5 +1,6 @@
 import json
 import os
+import warnings
 
 from dify_client import ChatClient
 
@@ -17,32 +18,40 @@ def user_say(what: str):
         inputs={}, query=what, user="dify_terminal", conversation_id=conversation_id, response_mode="streaming"
     )
     chat_response.raise_for_status()
-    accumulated_answer = ""
+
     for line in chat_response.iter_lines(decode_unicode=True):
         # line="event: ping" when starting the LLM
         if not line.startswith("data:"):
             continue
         data: str = line.split("data:", 1)[-1]
         if data := data.strip():
-            dic = json.loads(data)
+            dic: dict = json.loads(data)
 
             if conversation_id is None:
                 conversation_id = dic.get("conversation_id")
 
             if answer := dic.get("answer"):
-                accumulated_answer += answer
-                # ref: https://stackoverflow.com/a/3249539/11397457
-                print(accumulated_answer, end='\r', flush=True)
+                # ref: https://stackoverflow.com/questions/3249524/print-in-one-line-dynamically/3249539#3249539
+                print(answer, end="", flush=True)
+
+    # append a "\n" at the end of LLM response
+    print("")
 
 
 def main():
+    counter = 0
     while True:
-        query = input(": ")
-        if not query:
-            continue
-        user_say(query)
-        # start a new line
-        print("")
+        try:
+            query = input("> ")
+            if not query:
+                continue
+            print("-")
+            user_say(query)
+            counter += 1
+        except Exception as e:
+            warnings.warn(str(e), stacklevel=1)
+        finally:
+            print(f"--- {counter} ---")
 
 
 if __name__ == "__main__":
